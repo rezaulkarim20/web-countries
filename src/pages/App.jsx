5f10fb29eaadc7cc2adfd892841c40a3b5dc3fc4
rezaulkarim20 <rezaulkarim958672@gmail.com>
2024-10-03 20:40:34 +0600

    full website update

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,5 @@
 import Countries from "../components/Countries";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const App = () => {
